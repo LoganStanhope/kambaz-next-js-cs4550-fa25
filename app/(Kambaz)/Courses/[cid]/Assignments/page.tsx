a6471cefe532d4d55a6bdd62298106669ea965ba
LoganStanhope <lastanhope@outlook.com>
2025-09-17 00:04:36 -0400

    Complete Assignment page screen

--- app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -1,20 +1,42 @@
 import Link from "next/link";
 
-export default function Assignments() {
+export default async function Assignments({params,}:
+                                              { params: Promise<{ cid: string, aid: string }>; }) {
+    const {cid} = await params;
+    const {aid} = await params;
     return (
         <div id="wd-assignments">
             <input placeholder="Search for Assignments"
-                   id="wd-search-assignment" />
+                   id="wd-search-assignment"/>
             <button id="wd-add-assignment-group">+ Group</button>
             <button id="wd-add-assignment">+ Assignment</button>
-            <h3 id="wd-assignments-title">
-                ASSIGNMENTS 40% of Total <button>+</button> </h3>
-            <ul id="wd-assignment-list">
-                <li className="wd-assignment-list-item">
-                    <Link href="/Courses/1234/Assignments/123" id="wd-assignment-link">A1 - ENV + HTML</Link> </li>
-                <li className="wd-assignment-list-item">
-                    {/* Complete On Your Own */}
-                </li>
-            </ul>
+            <div>
+                <h3 id="wd-assignments-title">
+                    ASSIGNMENTS 40% of Total <button>+</button></h3>
+                <ul id="wd-assignment-list">
+                    <li className="wd-assignment-list-item">
+                        <Link href={`/Courses/${cid}/Assignments/${aid}`} id="wd-assignment-link">A1 - ENV + HTML</Link>
+                    </li>
+                    <div className="wd-assignment-list-item" id="wd-assignment-link">
+                        Multiple Modules | <b>Not available until</b> May 6 at 12:00am |<br/>
+                        <b>Due</b> May 13 at 11:59pm | 100pts
+                    </div>
+                    <li className="wd-assignment-list-item">
+                        <Link href={`/Courses/${cid}/Assignments/${aid}`} id="wd-assignment-link">A2 - CSS +
+                            BOOTSTRAP</Link></li>
+                    <div className="wd-assignment-list-item" id="wd-assignment-link">
+                        Multiple Modules | <b>Not available until</b> May 13 at 12:00am |<br/>
+                        <b>Due</b> May 20 at 11:59pm | 100pts
+                    </div>
+                    <li className="wd-assignment-list-item">
+                        <Link href={`/Courses/${cid}/Assignments/${aid}`} id="wd-assignment-link">A3 - JAVASCRIPT +
+                            REACT</Link></li>
+                    <div className="wd-assignment-list-item" id="wd-assignment-link">
+                        Multiple Modules | <b>Not available until</b> May 20 at 12:00am |<br/>
+                        <b>Due</b> May 27 at 11:59pm | 100pts
+                    </div>
+                </ul>
+            </div>
         </div>
-    );}
+    );
+}
--- app/(Kambaz)/index.tsx
+++ app/(Kambaz)/index.tsx
@@ -1,17 +1,20 @@
-import { Routes, Route, Navigate } from "react-router";
+import {Routes, Route, Navigate} from "react-router";
 import Assignments from "@/app/(Kambaz)/Courses/[cid]/Assignments/page";
 import Modules from "@/app/(Kambaz)/Courses/[cid]/Modules/page";
 import Home from "@/app/(Kambaz)/Courses/[cid]/Home/page";
 
-export default function Kambaz() {
+export default async function Kambaz({params,}:
+                                         { params: Promise<{ cid: string }>; }) {
+    await params;
     return (
         <div id="wd-kambaz">
             <h1>Kambaz</h1>
             <Routes>
-                <Route path="/" element={<Navigate to="Home" />} />
-                <Route path="Home" element={<Home />} />
-                <Route path="Modules" element={<Modules />} />
-                <Route path="Assignments" element={<Assignments />} />
+                <Route path="/" element={<Navigate to="Home"/>}/>
+                <Route path="Home" element={<Home/>}/>
+                <Route path="Modules" element={<Modules/>}/>
+                <Route path="Assignments" element={<Assignments params={params}/>}/>
             </Routes>
         </div>
-    );}
+    );
+}
