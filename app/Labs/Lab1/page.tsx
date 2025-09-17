d18c790ab5b0d954773d8ab602551fbcee7bd349
LoganStanhope <lastanhope@outlook.com>
2025-09-16 21:21:59 -0400

    Finished lab exercises

--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -4,92 +4,7 @@ export default function Lab1() {
         <div id={"wd-lab1"}>
             <h2>Lab 1</h2>
             <h3>HTML Examples</h3>
-
-            <h4 id="wd-dropdowns">Dropdowns</h4>
-            <h5>Select one</h5>
-            <label htmlFor="wd-select-one-genre">
-                Favorite movie genre: </label><br/>
-            <select id="wd-select-one-genre">
-                <option value="COMEDY">Comedy</option>
-                <option value="DRAMA">Drama</option>
-                <option selected value="SCIFI">
-                    Science Fiction</option>
-                <option value="FANTASY">Fantasy</option>
-            </select>
-
-
-            <h5>Select many</h5>
-            <label htmlFor="wd-select-many-genre">
-                Favorite all movie genres: </label><br/>
-            <select id="wd-select-many-genre" multiple>
-                <option selected value="COMEDY">Comedy</option>
-                <option value="DRAMA">Drama</option>
-                <option selected value="SCIFI">
-                    Science Fiction</option>
-                <option value="FANTASY">Fantasy</option>
-            </select>
-
-
-            <div id="wd-forms">
-                <h4>Form Elements</h4>
-
-                <h5 id="wd-radio-buttons">Radio buttons</h5>
-
-                <label>Favorite movie genre:</label><br />
-
-                <input type="radio" name="radio-genre" id="wd-radio-comedy"/>
-                <label htmlFor="wd-radio-comedy">Comedy</label><br />
-
-                <input type="radio" name="radio-genre" id="wd-radio-drama"/>
-                <label htmlFor="wd-radio-drama">Drama</label><br />
-
-                <input type="radio" name="radio-genre" id="wd-radio-scifi"/>
-                <label htmlFor="wd-radio-scifi">Science Fiction</label><br />
-
-                <input type="radio" name="radio-genre" id="wd-radio-fantasy"/>
-                <label htmlFor="wd-radio-fantasy">Fantasy</label>
-
-
-                <h5 id="wd-checkboxes">Checkboxes</h5>
-                <label>Favorite movie genre:</label><br/>
-
-                <input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
-                <label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
-
-                <input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
-                <label htmlFor="wd-chkbox-drama">Drama</label><br/>
-
-                <input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
-                <label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
-
-                <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
-                <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
-
-
-                {/*<form id="wd-text-fields">*/}
-                    <h5>Text Fields</h5>
-                    <label htmlFor="wd-text-fields-username">Username:</label>
-                    <input id="wd-text-fields-username" placeholder="jdoe"/> <br/>
-                    <label htmlFor="wd-text-fields-password">Password:</label>
-                    <input type="password" id="wd-text-fields-password" value="123@#$asd"/>
-                    <br/>
-                    <label htmlFor="wd-text-fields-first-name">First name:</label>
-                    <input type="text" id="wd-text-fields-first-name" title="John"/> <br/>
-                    <label htmlFor="wd-text-fields-last-name">Last name:</label>
-                    <input type="text" id="wd-text-fields-last-name" placeholder="Doe"
-                           value="Wonderland" title="The last name"/>
-                    {/* copy rest of form elements here  */}
-                {/*</form>*/}
-            </div>
-
-            <h5 id="wd-buttons">Buttons</h5>
-            <button id="wd-all-good"
-                    onClick={() => alert("Life is Good!")}
-                    type="button">
-                Hello World!
-            </button>
-
-
+            {/* -------------------- HEADING TAGS ---------------------*/}
             <div id="wd-h-tag">
                 <h4>Heading Tags</h4>
                 Text documents are often broken up into several sections and subsections. Each section is usually
@@ -100,6 +15,8 @@ export default function Lab1() {
                 plain text so that it renders in a browser as large headings. There are 6 heading tags for different
                 sizes: h1, h2, h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest heading.
             </div>
+            {/* --------------------------------------------------------*/}
+            {/* -------------------- PARAGRAPH TAGS ---------------------*/}
             <div id="wd-p-tag">
                 <h4>Paragraph Tag</h4>
                 <p id="wd-p-1"> ... </p>
@@ -118,6 +35,8 @@ export default function Lab1() {
                     tag to tell browsers to render the gaps.
                 </p>
             </div>
+            {/* --------------------------------------------------------*/}
+            {/* -------------------- LIST TAGS/FAVORITE RECIPE ---------------------*/}
             <div id="wd-lists">
                 <h4>List Tags</h4>
                 <h5>Ordered List Tag</h5>
@@ -132,76 +51,244 @@ export default function Lab1() {
                     <li>Flip and cook the other side.</li>
                     <li>Serve and enjoy!</li>
                 </ol>
+                My favorite recipe - Apple Crisp:
+                <ol id="wd-your-favorite-recipe">
+                    <li>Preheat oven to 350 degrees F.</li>
+                    <li>Cream 1 cup white sugar with 1 stick softened butter.</li>
+                    <li>Add in 1 cup of rolled oats and 1/2 cup flour.</li>
+                    <li>Toss diced and skinned apples with cinnamon and brown sugar.</li>
+                    <li>Place apples in a greased baking dish.</li>
+                    <li>Sprinkle the oat topping on the apples.</li>
+                    <li>Bake for 30-40 minutes or until topping is golden brown.</li>
+                    <li>Serve with some vanilla ice cream and whipped cream!</li>
+                </ol>
             </div>
+            {/* --------------------------------------------------------*/}
+            {/* -------------------- UNORDERED TAGS ---------------------*/}
+            <h5>Unordered List Tag</h5>
+            My favorite books (in no particular order)
+            <ul id="wd-my-books">
+                <li>Dune</li>
+                <li>Lord of the Rings</li>
+                <li>Ender's Game</li>
+                <li>Red Mars</li>
+                <li>The Forever War</li>
+            </ul>
+            Your favorite books (in no particular order)
+            <ul id="wd-your-books">
+                <li>Twilight</li>
+                <li>Act Your Age Eve Brown</li>
+                <li>A Court of Thorns and Roses</li>
+            </ul>
+            {/* --------------------------------------------------------*/}
+            {/* -------------------- TABLE TAGS ---------------------*/}
             <div id="wd-tables">
                 <h4>Table Tag</h4>
-                <table border={1} width="100%">    {/* table, border, width */}
-                    <thead>                    {/* table headings */}
-                    <tr>                        {/* table row */}
+                <table border={1} width="100%">
+                    <thead>
+                    <tr>
                         <th>Quiz</th>
-                        {/* table heading*/}
                         <th>Topic</th>
                         <th>Date</th>
                         <th>Grade</th>
                     </tr>
                     </thead>
-                    <tbody>                    {/* table body */}
-                    <tr>                        {/* table row */}
+                    <tbody>
+                    <tr>
                         <td>Q1</td>
-                        {/* table data / cell */}
                         <td>HTML</td>
-                        <td>2/3/21</td>
+                        <td>2/3/25</td>
                         <td>85</td>
                     </tr>
+                    <tr>
+                        <td>Q2</td>
+                        <td>CSS</td>
+                        <td>2/10/25</td>
+                        <td>90</td>
+                    </tr>
+                    <tr>
+                        <td>Q3</td>
+                        <td>React</td>
+                        <td>2/17/25</td>
+                        <td>82</td>
+                    </tr>
+                    <tr>
+                        <td>Q4</td>
+                        <td>JavaScript</td>
+                        <td>2/24/25</td>
+                        <td>99</td>
+                    </tr>
+                    <tr>
+                        <td>Q5</td>
+                        <td>GitHub</td>
+                        <td>2/31/25</td>
+                        <td>100</td>
+                    </tr>
+                    <tr>
+                        <td>Q6</td>
+                        <td>Kambaz</td>
+                        <td>3/6/25</td>
+                        <td>78</td>
+                    </tr>
+                    <tr>
+                        <td>Q7</td>
+                        <td>Vercel</td>
+                        <td>3/13/25</td>
+                        <td>93</td>
+                    </tr>
+                    <tr>
+                        <td>Q8</td>
+                        <td>Java</td>
+                        <td>3/20/25</td>
+                        <td>86</td>
+                    </tr>
+                    <tr>
+                        <td>Q9</td>
+                        <td>Python</td>
+                        <td>3/27/25</td>
+                        <td>90</td>
+                    </tr>
+                    <tr>
+                        <td>Q10</td>
+                        <td>Web Development</td>
+                        <td>4/3/25</td>
+                        <td>91</td>
+                    </tr>
                     </tbody>
-                    <tfoot>                    {/* table row */}
+                    <tfoot>
                     <tr>
                         <td colSpan={3}>Average</td>
-                        {/* column span */}
-                        <td>90</td>
+                        <td>84.9</td>
                     </tr>
                     </tfoot>
                 </table>
             </div>
+            {/* --------------------------------------------------------*/}
+            {/* -------------------- IMAGE TAGS ---------------------*/}
+            <div id="wd-images">
+                <h4>Image tag</h4>
+                Loading an image from the internet: <br/>
+                <img id="wd-starship" width="400px"
+                     src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                <br/>
+                Loading a local image:
+                <br/>
+                <img id="wd-teslabot" src="/images/teslabot.jpg" height="200px"/></div>
+            {/* --------------------------------------------------------*/}
+            {/* -------------------- FORMS TAGS ---------------------*/}
+            <div id="wd-forms">
+                <h4>Form Elements</h4>
+                <form id="wd-text-fields">
+                    {/* -------------------- TEXT FIELDS ---------------------*/}
+                    <h5>Text Fields</h5>
+                    <label htmlFor="wd-text-fields-username">Username:</label>
+                    <input id="wd-text-fields-username" placeholder="jdoe"/> <br/>
+                    <label htmlFor="wd-text-fields-password">Password:</label>
+                    <input type="password" id="wd-text-fields-password" value="123@#$asd"/>
+                    <br/>
+                    <label htmlFor="wd-text-fields-first-name">First name:</label>
+                    <input type="text" id="wd-text-fields-first-name" title="John"/> <br/>
+                    <label htmlFor="wd-text-fields-last-name">Last name:</label>
+                    <input type="text" id="wd-text-fields-last-name" placeholder="Doe"
+                           value="Wonderland" title="The last name"/>
+                </form>
+                {/* -------------------- TEXT BOXES ---------------------*/}
+                <h5>Text boxes</h5>
+                <label>Biography:</label><br/>
+                <textarea id="wd-textarea" cols={30} rows={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+                <h5 id="wd-buttons">Buttons</h5>
+                <button id="wd-all-good"
+                        onClick={() => alert("Life is Good!")}
+                        type="button">
+                    Hello World!
+                </button>
+                {/* -------------------- RADIO BUTTONS ---------------------*/}
+                <h5 id="wd-radio-buttons">Radio buttons</h5>
+                <label>Favorite movie genre:</label><br/>
+                <input type="radio" name="radio-genre" id="wd-radio-comedy"/>
+                <label htmlFor="wd-radio-comedy">Comedy</label><br/>
+                <input type="radio" name="radio-genre" id="wd-radio-drama"/>
+                <label htmlFor="wd-radio-drama">Drama</label><br/>
+                <input type="radio" name="radio-genre" id="wd-radio-scifi"/>
+                <label htmlFor="wd-radio-scifi">Science Fiction</label><br/>
+                <input type="radio" name="radio-genre" id="wd-radio-fantasy"/>
+                <label htmlFor="wd-radio-fantasy">Fantasy</label>
+                <h5 id="wd-checkboxes">Checkboxes</h5>
+                <label>Favorite movie genre:</label><br/>
+                {/* -------------------- CHECK BOXES ---------------------*/}
+                <input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
+                <label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
+                <input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
+                <label htmlFor="wd-chkbox-drama">Drama</label><br/>
+                <input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
+                <label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
+                <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
+                <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
+                {/* -------------------- DROPDOWNS ---------------------*/}
+                <h4 id="wd-dropdowns">Dropdowns</h4>
+                <h5>Select one</h5>
+                <label htmlFor="wd-select-one-genre">
+                    Favorite movie genre: </label><br/>
+                <select id="wd-select-one-genre">
+                    <option value="COMEDY">Comedy</option>
+                    <option value="DRAMA">Drama</option>
+                    <option selected value="SCIFI">
+                        Science Fiction
+                    </option>
+                    <option value="FANTASY">Fantasy</option>
+                </select>
+                {/* -------------------- SELECT MANY BOXES ---------------------*/}
+                <h5>Select many</h5>
+                <label htmlFor="wd-select-many-genre">
+                    Favorite all movie genres: </label><br/>
+                <select id="wd-select-many-genre" multiple>
+                    <option selected value="COMEDY">Comedy</option>
+                    <option value="DRAMA">Drama</option>
+                    <option selected value="SCIFI">
+                        Science Fiction
+                    </option>
+                    <option value="FANTASY">Fantasy</option>
+                </select>
+                {/* --------------------------------------------------------*/}
+                {/* -------------------- OTHER HTML FIELDS ---------------------*/}
+                <h4>Other HTML field types</h4>
+                <form id="wd-other-html-fields">
+                <label htmlFor="wd-text-fields-email"> Email: </label>
+                <input type="email"
+                       placeholder="jdoe@somewhere.com"
+                       id="wd-text-fields-email"/><br/>
+
+                <label htmlFor="wd-text-fields-salary-start"> Starting salary:</label>
+                <input type="number"
+                       value="100000"
+                       placeholder="1000"
+                       id="wd-text-fields-salary-start"/><br/>
+
+                <label htmlFor="wd-text-fields-rating"> Rating: </label>
+                <input type="range"
+                       value="4"
+                       max="5"
+                       placeholder="Doe"
+                       id="wd-text-fields-rating"/><br/>
+
+                <label htmlFor="wd-text-fields-dob"> Date of birth: </label>
+                <input type="date"
+                       value="2000-01-21"
+                       id="wd-text-fields-dob"/><br/>
+                </form>
+            </div>
+            {/* --------------------------------------------------------*/}
+            {/* -------------------- HTML ANCHOR TAGS ---------------------*/}
+            <h4>Anchor tag</h4>
+            Please
+            <a href="https://www.lipsum.com" id="wd-lipsum"> click here </a>
+            to get dummy text<br/>
+            And
+            <a href="https://github.com/LoganStanhope/kambaz-next-js-cs4550-fa25" id="wd-github"> click here </a>
+            to view my code repository on GitHub<br/>
 
 
-            <h4>Other HTML field types</h4>
 
-            <label htmlFor="wd-text-fields-salary-start">
-                Starting salary:</label>
-            <input type="number"
-                   id="wd-text-fields-salary-start"
-                   placeholder="1000"
-                   value="100000"/><br/>
-
-            <label htmlFor="wd-text-fields-rating">
-                Rating: </label>
-            <input type="range" id="wd-text-fields-rating"
-                   placeholder="Doe"
-                   max="5"
-                   value="4"/><br/>
-
-            <label htmlFor="wd-text-fields-email">
-                Email: </label>
-            <input type="email"
-                   placeholder="jdoe@somewhere.com"
-                   id="wd-text-fields-email"/><br/>
-
-            <label htmlFor="wd-text-fields-dob">
-                Date of birth: </label>
-            <input type="date"
-                   id="wd-text-fields-dob"
-                   value="2000-01-21"/><br/>
-
-            <label
-                htmlFor="dobFld">
-                Date of Birth
-            </label>
-
-            <input
-                type="date"
-                id="dobFld"
-                value="2011-11-22"/>
         </div>
     );
 }
